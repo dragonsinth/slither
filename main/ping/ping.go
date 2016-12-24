b34de91da0686ca0e19ab0af725d014ee3647ec7
Scott Blum <dragonsinth@gmail.com>
2016-12-24 13:55:13 -0500

    Improve pinger

--- main/ping/ping.go
+++ main/ping/ping.go
@@ -7,23 +7,29 @@ import (
 	"net"
 	"os"
 	"sort"
+	"sync"
 	"time"
 )
 
 type result struct {
-	addr string
-	dur  time.Duration
+	addr            string
+	min, max, total time.Duration
+	succ, fail      int
 }
 
-type byDurationAscending []result
+func (r *result) ave() time.Duration {
+	return r.total / time.Duration(r.succ)
+}
 
-func (p byDurationAscending) Len() int {
+type byAveDurationAscending []result
+
+func (p byAveDurationAscending) Len() int {
 	return len(p)
 }
-func (p byDurationAscending) Less(i, j int) bool {
-	return p[i].dur < p[j].dur
+func (p byAveDurationAscending) Less(i, j int) bool {
+	return p[i].ave() < p[j].ave()
 }
-func (p byDurationAscending) Swap(i, j int) {
+func (p byAveDurationAscending) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
 
@@ -34,25 +40,50 @@ func main() {
 	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
 	scanner.Split(bufio.ScanLines)
 
-	results := []result{}
+	var wg sync.WaitGroup
+
+	chResult := make(chan result)
 	for scanner.Scan() {
-		addr := scanner.Text()
-		start := time.Now()
-		if conn, err := net.DialTimeout("tcp", addr, 1*time.Second); err != nil {
-			log.Printf("addr=%s err=%s\n", addr, err.Error())
-		} else {
-			dur := time.Now().Sub(start)
-			log.Printf("addr=%s time=%s", addr, dur.String())
-			results = append(results, result{
+		wg.Add(1)
+		go func(addr string) {
+			defer wg.Done()
+			r := result{
 				addr: addr,
-				dur:  dur,
-			})
-			conn.Close()
-		}
+			}
+			for i := 0; i < 5; i++ {
+				start := time.Now()
+				if conn, err := net.DialTimeout("tcp", addr, 1*time.Second); err != nil {
+					log.Printf("addr=%s err=%s\n", addr, err.Error())
+					r.fail++
+				} else {
+					dur := time.Since(start)
+					conn.Close()
+					if r.succ == 0 || dur < r.min {
+						r.min = dur
+					}
+					if r.succ == 0 || dur > r.max {
+						r.max = dur
+					}
+					r.total += dur
+					r.succ++
+				}
+			}
+			chResult <- r
+		}(scanner.Text())
 	}
 
-	sort.Sort(byDurationAscending(results))
+	results := []result{}
+	go func() {
+		for r := range chResult {
+			results = append(results, r)
+		}
+	}()
+
+	wg.Wait()
+	close(chResult)
+
+	sort.Sort(byAveDurationAscending(results))
 	for _, r := range results {
-		fmt.Printf("addr=%s time=%s\n", r.addr, r.dur.String())
+		fmt.Printf("addr=%s min=%s max=%s ave=%s succ=%d fail=%d\n", r.addr, r.min, r.max, r.ave(), r.succ, r.fail)
 	}
 }
